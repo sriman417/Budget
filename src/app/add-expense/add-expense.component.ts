b12b43c5d70fc973a38c4c3fd16684eaa3f6ce16
Nainala Srimannarayana <105102064+sriman417@users.noreply.github.com>
2024-07-13 16:04:24 +0530

    Update add-expense.component.ts

--- src/app/add-expense/add-expense.component.ts
+++ src/app/add-expense/add-expense.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BudgetService, BudgetItem } from '../budget.service';
+import { BudgetService } from '../budget.service';
 
 @Component({
   selector: 'app-add-expense',
@@ -9,25 +9,20 @@ import { BudgetService, BudgetItem } from '../budget.service';
 export class AddExpenseComponent {
   description = '';
   amount = 0;
-  datetime: string = '';
+  datetime = '';
+  recurrenceType = ''; 
   budgetService: BudgetService;
-
   constructor(budgetService: BudgetService) {
     this.budgetService = budgetService;
   }
 
   addExpense() {
-    if (this.description && this.amount) {
-      const newItem: BudgetItem = {
-        id: Date.now(),
-        description: this.description,
-        amount: this.amount,
-        datetime: this.datetime
-      };
-      this.budgetService.addItem(newItem);
+    if (this.description && this.amount && this.datetime && this.recurrenceType) {
+      this.budgetService.addItem(this.description, this.amount, this.datetime, this.recurrenceType);
       this.description = '';
       this.amount = 0;
-      this.datetime = '';
+      this.datetime = ''; 
+      this.recurrenceType = '';
     }
   }
 }
